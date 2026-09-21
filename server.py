cc6741a399d2402a7abb30a799ebc3125927db1d
sjekketing-maker <sjekketing@gmail.com>
2026-09-21 19:54:52 +0200

    Update server.py

--- server.py
+++ server.py
@@ -36,5 +36,5 @@ def download():
     return send_file(latest_file, as_attachment=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
